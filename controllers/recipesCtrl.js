90e7e352ab6559d10c67381b17e551c6be365cb6
samleemcknight <samleemcknight@gmail.com>
2021-04-07 20:57:00 -0600

    updates recipes methods

--- controllers/recipesCtrl.js
+++ controllers/recipesCtrl.js
@@ -4,8 +4,18 @@ const urlIngredients = `https://api.spoonacular.com/recipes/findByIngredients`;
 const urlRecipe = `https://api.spoonacular.com/recipes/`
 const db = require('../models');
 
-const index = (req, res) => {
-  res.send(recipesJson);
+// for showing recipes in cookbook
+const index = async (req, res) => {
+  try {
+    const foundUser = await db.user.findOne({
+      where : {id: 1},
+      include: [db.recipe]
+    })
+    res.json({ status: 201, message: "success", recipes: foundUser.recipes })
+  } catch (error) {
+    return res.json({ status: 401, message: "error", error })
+  }
+
 };
 
 const find = async (req, res) => {
@@ -13,10 +23,12 @@ const find = async (req, res) => {
     where : {id: 1},
     include: [db.ingredient]
   })
+  // format user's ingredients into friendly version for api request
   let ingredients = []
   for (let i=0; i<foundUser.ingredients.length; i++) {
     ingredients.push(foundUser.ingredients[i].name)
   }
+  // api request
   axios.get(urlIngredients, {
       params: {
         apiKey: process.env.API_KEY,
@@ -48,31 +60,25 @@ const show = (req, res) => {
 // add recipe on '/show' to db favorites
 const favorite = async (req, res) => {
   // find user 
-  const foundUser = await db.user.findOne({ where: { id: req.body.user.id } })
+  const foundUser = await db.user.findOne({ where: { id: 1 } })
   //in case no user is found
   if (!foundUser) {
    return res.json({ message: "No user found"})
   }
   
-  // const ingredients = []
-  // // push ingredients into array
-  // for (let i = 0; i < req.body.extendedIngredients.length; i++) {
-  //   ingredients.push(req.body.extendedIngredients[i].original)
-  // }
-  // // make array into string
-  // ingredients = ingredients.join(', ')
-  
   // create new recipe
-  const newRecipe = await db.recipe.create({
-    title: req.body.title,
-    instructions: req.body.instructions,
-    image_url: req.body.image,
-    ingredients: req.body.extendedIngredients.join(', ')
-  }).catch(err => res.json({status: 400, message: "error creating recipe", error: err}))
+  const [newRecipe, created] = await db.recipe.findOrCreate({
+    where: { title: req.body.title},
+    defaults: {
+      title: req.body.title,
+      instructions: req.body.instructions,
+      image_url: req.body.image_url,
+      ingredients: req.body.ingredients
+    }
+  })
 
   // associate recipe with user
-  const makeAssoc = await foundUser.createRecipe(newRecipe).catch(err => console.log(err, "error"))
-  // for some reason, createRecipe hasn't been adding the assoc, so this will manually do so:
+  
   foundUser.addRecipe(newRecipe).then(response => {
     foundUser.getRecipes().then(recipes => {
       return res.status(201).json({ status: 201, message: "success", recipes })
