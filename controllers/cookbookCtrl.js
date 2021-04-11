2e5d979e5f7f6e7a512bb1199f64cbb7440eee00
samleemcknight <samleemcknight@gmail.com>
2021-04-11 12:10:51 -0600

    updates update method

--- controllers/cookbookCtrl.js
+++ controllers/cookbookCtrl.js
@@ -1,5 +1,6 @@
 const db = require('../models');
-const {Op} = require('sequelize')
+const {Op} = require('sequelize');
+const { success } = require('../passport/localStrategy');
 
 // for showing recipes in cookbook
 const index = async (req, res) => {
@@ -47,8 +48,27 @@ const deleteRecipe = async (req, res) => {
   }
 }
 
+const editRecipe = async (req, res) => {
+  try {
+    const recipe = await db.recipe.findOne({where: {id: req.body.id}})
+    
+    const updateRecipe = await recipe.update({
+      title: req.body.title,
+      ingredients: req.body.ingredients,
+      instructions: req.body.instructions,
+      image_url: req.body.imageUrl
+    })
+    
+    return res.json( {message: "success", updateRecipe} )
+    
+  } catch (error) {
+    return res.json({ status: 401, message: "error editing recipe", error })
+  }
+}
+
 module.exports = {
   index,
   show,
-  deleteRecipe
+  deleteRecipe,
+  editRecipe
 };
--- routes/cookbook.js
+++ routes/cookbook.js
@@ -6,6 +6,7 @@ const ctrl = require("../controllers");
 router.get("/", ctrl.cookbookCtrl.index);
 router.get("/show/:id", ctrl.cookbookCtrl.show);
 router.delete("/delete/:id", ctrl.cookbookCtrl.deleteRecipe)
+router.put("/edit", ctrl.cookbookCtrl.editRecipe)
 
 // exports
 module.exports = router;
